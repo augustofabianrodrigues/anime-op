0aef42696913df4f3535a2f087cad77ba19aedb4
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-09-30 18:12:12 -0300

    feat: Develop home page UI

--- web-components/src/direflow-components/anime-op-app/components/home/search/SearchBar.tsx
+++ web-components/src/direflow-components/anime-op-app/components/home/search/SearchBar.tsx
@@ -2,12 +2,14 @@ import { Styled } from 'direflow-component';
 import React, { FC } from 'react';
 import styles from './SearchBar.less';
 import SearchInput from './SearchInput';
+import ToggleFiltersButton from './ToggleFiltersButton';
 
 const SearchBar: FC = () => {
   return (
     <Styled styles={styles}>
       <section className="search-bar">
         <SearchInput />
+        <ToggleFiltersButton />
       </section>
     </Styled>
   );
