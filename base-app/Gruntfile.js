2e8dc08b2b2013c65f1cb064e9227e7e33471f8e
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-10-07 15:28:08 -0300

    chore: Improve build and minification

--- base-app/Gruntfile.js
+++ base-app/Gruntfile.js
@@ -4,16 +4,13 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    uglify: {
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
-        // the banner is inserted at the top of the output
-        banner:
-          '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>'],
+        globals: {
+          jQuery: true,
         },
+        ignores: ['src/js/libs/**/*.js'],
       },
     },
 
@@ -38,29 +35,87 @@ module.exports = function (grunt) {
       },
     },
 
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true,
-        },
-        ignores: ['src/js/libs/**/*.js'],
-      },
-    },
-
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['build'],
       options: {
-        atBegin: true
-      }
+        atBegin: true,
+      },
+    },
+
+    clean: {
+      build: ['build'],
+    },
+
+    copy: {
+      build: {
+        expand: true,
+        cwd: 'public',
+        src: '**',
+        dest: 'build/',
+      },
+    },
+
+    htmlmin: {
+      build: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          'build/index.html': 'build/index.html',
+        },
+      },
+    },
+
+    uglify: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'build/js',
+            src: '**/*.js',
+            dest: 'build/js',
+            rename: function (dst, src) {
+              return dst + '/' + src;
+            },
+          },
+        ],
+      },
+    },
+
+    cacheBust: {
+      build: {
+        options: {
+          assets: ['js/**'],
+          baseDir: './build/',
+          deleteOriginals: true,
+        },
+        cwd: 'build/',
+        src: ['index.html', 'js/*'],
+        urlPrefixes: ['js/main.bundle.js', 'js/require.js'],
+      },
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-cache-bust');
 
-  grunt.registerTask('build', ['jshint', 'requirejs:' + target]);
+  var buildSteps = ['jshint', 'requirejs:' + target];
+
+  if (target === 'production') {
+    buildSteps.push('clean:build');
+    buildSteps.push('copy:build');
+    buildSteps.push('htmlmin:build');
+    buildSteps.push('uglify:build');
+    buildSteps.push('cacheBust:build');
+  }
+
+  grunt.registerTask('build', buildSteps);
 };
